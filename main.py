973a246ac8be3c92f4c3507a1c4835c424b11b32
snowyfloke <yasmin@snowfloke.com>
2026-04-01 09:37:31 -0300

    Added basic music and audio-related commands

--- main.py
+++ main.py
@@ -3,23 +3,98 @@ import time
 import threading
 import logging
 import discord
+import asyncio
 
 from discord.ext import tasks
 from discord.ext import commands
+from discord import FFmpegPCMAudio
+from discord import FFmpegAudio
+from discord import FFmpegOpusAudio
+from discord import Color
 from dotenv import load_dotenv
 
+from song import get_audio
 
 # Discord Bot Auth
 load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 intents.message_content = True
-bot = commands.Bot(command_prefix="!", intents=intents)
+intents.members = True
+bot = commands.Bot(command_prefix = "!", help_command=None, intents = intents)
 # The bot later gets activated by the line "bot.run(DISCORD_TOKEN)"
+
+@bot.event
+async def on_ready():
+    try: # If bot can connect to discord
+
+        print('Disord bot succesfully connected!')
+    except Exception as e:
+        print(f"[!] couldn't connect, an Error occured! Error: {e}")
 
 # Ping command, useful for testing purposes, and learning how to make new commands :3
 @bot.command()
 async def ping(ctx):
     await ctx.send(f"Pong! | {round(bot.latency * 1000)}ms!")
+
+# Voice Channel Commands (Useful for other future commands)
+@bot.command()
+async def join(ctx):
+    if ctx.author.voice:
+        channel = ctx.message.author.voice.channel
+        try:
+            await channel.connect()
+            await ctx.send(f"Joined {channel.name}")
+        except Exception as e:
+            await ctx.send(f"Error: {e}")
+    else:
+        await ctx.send("You're not in a voice channel.")
+
+@bot.command()
+async def leave(ctx):
+    if ctx.voice_client:
+        channel = ctx.voice_client.channel
+        await ctx.guild.voice_client.disconnect()
+        await ctx.send(f"Left channel {channel.name}")
+    else:
+        await ctx.send("I'm not in a voice_channel.")
+
+# Song Commands
+@bot.command()
+async def play(ctx,*,query):
+    if ctx.author.voice is None:
+        await ctx.send("You're not in a voice channel.")
+    if ctx.voice_client is None:
+        channel = ctx.message.author.voice.channel
+        try:
+            await channel.connect()
+        except Exception as e:
+            await ctx.send(f"Error: {e}")
+    url, title = get_audio(query)
+
+    ctx.voice_client.play(discord.FFmpegPCMAudio(url))
+    await ctx.send(f"Now Playing: {title}")
+
+@bot.command()
+async def pause(ctx):
+    if ctx.voice_client is None:
+        await ctx.send("I'm not playing anything.")
+    if ctx.author.voice is None:
+        await ctx.send("You're not in a voice channel.")
+    if ctx.voice_client.is_paused():
+        await ctx.send("I already paused the Music.")
+    if ctx.voice_client.is_playing():
+        ctx.voice_client.pause()
+        await ctx.send("Paused the Music.")
+
+@bot.command()
+async def stop(ctx):
+    if ctx.voice_client is None:
+        await ctx.send("I'm not playing anything.")
+    elif ctx.author.voice is None:
+        await ctx.send("You're not in a voice channel.")
+    else:
+        ctx.voice_client.stop()
+
 # Bot Innit
 bot.run(DISCORD_TOKEN)
