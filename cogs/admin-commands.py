dcd2cbc20a5b41f6ebefb6b5328ade99a92c9734
snowyfloke <yasmin@snowfloke.com>
2026-07-05 16:25:19 -0300

    Added translation support for this branch

--- cogs/admin-commands.py
+++ cogs/admin-commands.py
@@ -2,12 +2,13 @@ import discord
 import datetime
 from discord.ext import commands
 from discord import app_commands
-from lang import get_user_lang
+from lang import get_user_lang, get_msg
+
 
 class Admin(commands.Cog):
     """
-        Server Management Section
-        Most commands require admin permissions!
+    Server Management Section
+    Most commands require admin permissions!
     """
 
     def __init__(self, bot):
@@ -15,62 +16,75 @@ class Admin(commands.Cog):
 
     @commands.hybrid_command()
     @commands.has_permissions(ban_members=True)
-    @app_commands.describe(member="Member to be banned, cannot be an admin", reason="Reason to ban, defaults to None")
+    @app_commands.describe(
+        member="Member to be banned, cannot be an admin",
+        reason="Reason to ban, defaults to None",
+    )
     async def ban(self, ctx, member: discord.Member, *, reason=None):
         """
-            Bans a member!
+        Bans a member!
 
-            Syntax: !ban @user-to-ban <reason>
-            If no reason is defined, it defaults to none.
+        Syntax: !ban @user-to-ban <reason>
+        If no reason is defined, it defaults to none.
         """
-        print("User typed !ban") # LOG
+        print("User typed !ban")  # LOG
         lang = get_user_lang(ctx.author.id)
         if member.guild_permissions.administrator:
-            msg = "Você está tentando banir um admin, n faça isso, ele ficará triste :(" if lang == "pt" else "You're trying to ban an admin, please don't do that, he will be very sad :("
+            msg = get_msg(lang, "ban_admin")
             await ctx.send(msg)
             return
 
         else:
             await member.ban(reason=reason)
-            msg = f"{member} foi banido por {ctx.author}! \n \nMotivo: {reason}" if lang == "pt" else f"{member} has been banned by {ctx.author}! \n \nReason: {reason}"
+            msg = get_msg(lang, "ban_member").format(
+                member_banned=member, author=ctx.author, reason_to_ban=reason
+            )
             await ctx.send(msg)
 
     @commands.hybrid_command(aliases=["expulsar"])
     @commands.has_permissions(kick_members=True)
-    @app_commands.describe(member="Member to be kicked out, cannot be an admin", reason="Reason to kick, defaults to None")
+    @app_commands.describe(
+        member="Member to be kicked out, cannot be an admin",
+        reason="Reason to kick, defaults to None",
+    )
     async def kick(self, ctx, member: discord.Member, reason=None):
         """
-            Kicks a member!
+        Kicks a member!
 
-            Syntax: !kick @user-to-kick <reason>
-            If no reason is defined, it defaults to none.
+        Syntax: !kick @user-to-kick <reason>
+        If no reason is defined, it defaults to none.
         """
-        print("User typed !kick") # LOG
+        print("User typed !kick")  # LOG
         lang = get_user_lang(ctx.author.id)
         if member.guild_permissions.administrator:
-            msg = "Você quer chutar um ADM? Que feio!" if lang == "pt" else "You want to kick an admin? MEAN!"
+            msg = get_msg(lang, "kick_admin")
             await ctx.send(msg)
             return
         else:
             await member.kick(reason=reason)
-            msg = f"{member} foi expulso por {ctx.author}! \n \n Motivo: {reason}" if lang == "pt" else f"{member} was kicked out by {ctx.author}! \n \n Reason: {reason}"
+            msg = get_msg(lang, "kick_member").format(
+                member_kicked=member, author=ctx.author, reason_to_kick=reason
+            )
             await ctx.send(msg)
 
     @commands.hybrid_command(aliases=["to", "castigo", "disciplinar"])
     @commands.has_permissions(moderate_members=True)
-    @app_commands.describe(member="Member to be timeouted", time="Ammount to timeout, s Seconds, m Minutes, h Hours, d Days, defaults to 60m")
+    @app_commands.describe(
+        member="Member to be timeouted",
+        time="Ammount to timeout, s Seconds, m Minutes, h Hours, d Days, defaults to 60m",
+    )
     async def timeout(self, ctx, member: discord.Member, *, time: str = "60m"):
         """
-            Timeouts a member!
+        Timeouts a member!
 
-            Syntax: !timeout @user-to-mute <time>
-            If no time is specified, it defaults to 60 minutes.
+        Syntax: !timeout @user-to-mute <time>
+        If no time is specified, it defaults to 60 minutes.
 
-            Examples:
-            !timeout @user 10 s | !timeout @user 5m | !timeout @user 2h | !timeout @user 1d
+        Examples:
+        !timeout @user 10s | !timeout @user 5m | !timeout @user 2h | !timeout @user 1d
         """
 
-        print("User typed !mute") # LOG
+        print("User typed !mute")  # LOG
 
         lang = get_user_lang(ctx.author.id)
 
@@ -79,16 +93,17 @@ class Admin(commands.Cog):
         number = time[:-1]
 
         if unit not in units or not number.isdigit():
-            msg = "Formato invalido! \n \n Síntaxe: !mute @user 60s \n \n Unidades Suportadas: \n \n s Segundos | m Minutos | h Horas | d Dias" if lang == "pt" else "Invalid Format! \n \n Syntax: !mute @user 60s \n \n Units Supported: \n \n s Seconds | m Minutes | h Hours | d Days"
-            await ctx.send(msg)
+            syntax_msg = get_msg(lang, "wrong_syntax").format(syntax="!timeout <@user> 60s")
+            units_msg = get_msg(lang, "supported_units").format(units="s, m, h, d")
+            await ctx.send(f"{syntax_msg}\n\n{units_msg}")
             return
 
         seconds = int(number) * units[unit]
         duration = datetime.timedelta(seconds=seconds)
 
-        await  member.timeout(duration)
+        await member.timeout(duration)
 
-        msg = f"{member.name} foi pro cantinho da disciplina por {time}!" if lang == "pt" else f"{member.name} was sent to timeout for {time}!"
+        msg = get_msg(lang, "timeout").format(member_timeouted=member.name, time=time)
         await ctx.send(msg)
 
     @commands.hybrid_command(aliases=["deletar"])
@@ -96,33 +111,36 @@ class Admin(commands.Cog):
     @app_commands.describe(ammount="Number of messages to purge, defaults to 100")
     async def purge(self, ctx, ammount: int = 100):
         """
-            Deletes the n most recent messages
+        Deletes the n most recent messages
 
-            Syntax: !purge <ammount>
-            If no ammount is defined, defaults to 100.
+        Syntax: !purge <ammount>
+        If no ammount is defined, defaults to 100.
         """
-        print(f"User typed !purge {ammount}") # LOG
-        await ctx.channel.purge(limit=ammount + 1) # +1 needed to clear the ammount + the author message
+        print(f"User typed !purge {ammount}")  # LOG
+        await ctx.channel.purge(
+            limit=ammount + 1
+        )  # +1 needed to clear the ammount + the author message
 
     @commands.hybrid_command(aliases=["foto-de-perfil", "avatar"])
     @app_commands.describe(member="Member to grab the pfp, defaults to you!")
     async def pfp(self, ctx, member: discord.Member = None):
         """
-            Sends the pfp of a member as a .jpeg or .gif!
+        Sends the pfp of a member as a .jpeg or .gif!
 
-            Syntax: !pfp @member
+        Syntax: !pfp @member
         """
         if member == None:
             member = ctx.author
 
         icon_url = member.display_avatar.url
-        avatarEmbed = discord.Embed(title = f"{member.name}'s Avatar", color = 0xFFA500)
+        avatarEmbed = discord.Embed(title=f"{member.name}'s Avatar", color=0xFFA500)
 
-        avatarEmbed.set_image(url = f"{icon_url}")
+        avatarEmbed.set_image(url=f"{icon_url}")
 
         avatarEmbed.timestamp = ctx.message.created_at
 
-        await ctx.send(embed = avatarEmbed)
+        await ctx.send(embed=avatarEmbed)
+
 
 async def setup(bot):
     await bot.add_cog(Admin(bot))
